621406fbc121737022bfb719e7d8a0f581783f18
Валентина Лисина <lisinavalentina97@gmail.com>
2023-09-22 00:09:46 +0300

    before the review

---
+++ ya_news/news/pytest_tests/conftest.py
@@ -0,0 +1,93 @@
+import datetime
+
+import pytest
+from django.utils import timezone
+
+from news.models import News, Comment
+from yanews import settings
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def reader(django_user_model):
+    return django_user_model.objects.create(username='Читатель')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def reader_client(reader, client):
+    client.force_login(reader)
+    return client
+
+
+@pytest.fixture
+def news():
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст'
+    )
+    return news
+
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def news_pk_for_args(news):
+    return news.pk,
+
+
+@pytest.fixture
+def comment_pk_for_args(comment):
+    return comment.pk,
+
+
+today = datetime.datetime.today()
+
+
+@pytest.fixture
+def news_list():
+    all_news = [News(title=f'Новость {index}',
+                     text=f'Просто текст {index}',
+                     date=today - datetime.timedelta(index))
+                for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)]
+    news = News.objects.bulk_create(all_news)
+    return news
+
+
+@pytest.fixture
+def form_data(news):
+    return {
+        'news': news,
+        'text': 'Новый текст',
+        'created': timezone.now()
+    }
+
+
+@pytest.fixture
+def comments(news, author):
+    comments_list = [Comment(
+        news=news,
+        text='Текст',
+        created=today + timezone.timedelta(days=index),
+        author=author
+    )
+        for index in range(2)]
+    all_comments = Comment.objects.bulk_create(comments_list)
+    return all_comments
---
+++ ya_note/notes/tests/test_logic.py
@@ -0,0 +1,105 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from pytils.translit import slugify
+
+from notes.forms import WARNING
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestNoteCreateEditDelete(TestCase):
+    BASE_TITLE = 'Заголовок'
+    NEW_TITLE = 'Новый заголовок'
+    BASE_TEXT = 'Текст комментария'
+    NEW_TEXT = 'Новый текст комментария'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='author')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+
+        cls.reader = User.objects.create(username='reader')
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
+
+        cls.note = Note.objects.create(
+            title=cls.BASE_TITLE,
+            text=cls.BASE_TEXT,
+            author=cls.author
+        )
+
+        cls.success_url = reverse('notes:success')
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.add_url = reverse('notes:add')
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+
+        cls.form_data = {
+            'title': cls.NEW_TITLE,
+            'text': cls.NEW_TEXT,
+            'slug': 'form-slug'
+        }
+
+    def test_anonymous_user_cant_create_note(self):
+        response = self.client.post(self.add_url, data=self.form_data)
+        login_url = reverse('users:login')
+        expected_url = f'{login_url}?next={self.add_url}'
+        self.assertRedirects(response, expected_url)
+        self.assertEquals(Note.objects.count(), 1)
+
+    def test_user_can_create_note(self):
+        response = self.reader_client.post(self.add_url, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        self.assertEquals(Note.objects.count(), 2)
+        note = Note.objects.get(slug=self.form_data['slug'])
+        self.assertEquals(note.text, self.form_data['text'])
+        self.assertEquals(note.title, self.form_data['title'])
+        self.assertEquals(note.author, self.reader)
+
+    def test_unique_slug(self):
+        self.form_data['slug'] = self.note.slug
+        response = self.reader_client.post(
+            self.add_url, data=self.form_data
+        )
+        self.assertFormError(response,
+                             'form',
+                             'slug',
+                             errors=self.note.slug + WARNING)
+        self.assertEquals(Note.objects.count(), 1)
+
+    def test_empty_slug(self):
+        del self.form_data['slug']
+        expected_slug = slugify(self.form_data['title'])
+        response = self.reader_client.post(self.add_url, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        self.assertEquals(Note.objects.count(), 2)
+        note = Note.objects.last()
+        self.assertEquals(note.slug, expected_slug)
+
+    def test_author_can_edit_note(self):
+        response = self.author_client.post(self.edit_url, data=self.form_data)
+        self.assertRedirects(response, self.success_url)
+        self.note.refresh_from_db()
+        self.assertEquals(self.note.text, self.NEW_TEXT)
+
+    def test_reader_cant_edit_note(self):
+        response = self.reader_client.post(self.edit_url, data=self.form_data)
+        self.assertEquals(response.status_code, HTTPStatus.NOT_FOUND)
+        self.note.refresh_from_db()
+        self.assertEquals(self.note.text, self.BASE_TEXT)
+
+    def test_author_can_delete_note(self):
+        response = self.author_client.delete(self.delete_url)
+        self.assertRedirects(response, self.success_url)
+        notes_count = Note.objects.count()
+        self.assertEquals(notes_count, 0)
+
+    def test_reader_cant_delete_note(self):
+        response = self.reader_client.delete(self.delete_url)
+        self.assertEquals(response.status_code, HTTPStatus.NOT_FOUND)
+        notes_count = Note.objects.count()
+        self.assertEquals(notes_count, 1)
